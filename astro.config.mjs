ea265bec530a2b69974d45c0abec2d175976c48c
wiv404 <wivvz@yandex.ru>
2024-05-01 14:11:15 +0300

    simple page for my strapi test project + simple auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), auth()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
